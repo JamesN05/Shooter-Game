c92fd9ddd136c7218f81afc9dee0a2985900f0f5
James Nagle <G00427970@atu.ie>
2025-06-22 19:05:30 +0100

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Shooter_Game
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
